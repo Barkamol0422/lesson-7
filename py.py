a1b742c2992fc9c5e2fb0b2bf994d6be634d9906
Barkamol0422 <ganijonovbarkamol04222011@gmail.com>
2024-11-02 20:42:37 +0500

    Add files via upload

---
+++ py.py
@@ -0,0 +1,7 @@
+a=int(input('Enter a number'))
+b=int(input('Enter a number'))
+c=int(input('Enter a number'))
+if a>0 and b>0 and c>0:
+    print('This numbers are greator than 0')
+else:
+    print('They are smaller than 0')
